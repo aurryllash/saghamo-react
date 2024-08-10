4c3ba6ae1a25dcace88a8c519f40c50fb5cc3b27
aurryllash <lashatsiklauri77@gmail.com>
2024-08-10 18:20:19 +0400

    Add tailwind navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home'
+import Products from './components/pages/Products'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
     <div className="app">
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home /> } />
+          <Route path='/Products' element={<Products /> } />
         </Routes>
       </Router>
     </div>
