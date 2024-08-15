6a0b1fb9a9f13827c195a37ac685e3545cce7981
aurryllash <lashatsiklauri77@gmail.com>
2024-08-15 18:38:00 +0400

    Do registation form, using react-hook-form for forms validation

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,32 +1,95 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 
 const RegistrationForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div className="registration h-[100vh] flex justify-center items-start pt-20 bg-slate-600">
-      <form className="registration_form">
+      <form className="registration_form" onSubmit={handleSubmit(onSubmit)}>
         <div className="input-field">
           <label>
             UserName: <sup>*</sup>
           </label>
-          <input type="text" name="userName" placeholder="john..." />
+          <input
+            type="text"
+            {...register("userName", { required: "Field is required!" })}
+            placeholder="john..."
+          />
         </div>
+        <div className="registration_errors">
+          {errors.userName?.message && (
+            <p className="text-red-500">{String(errors.userName.message)}</p>
+          )}
+        </div>
+
         <div className="input-field">
           <label>
             Email: <sup>*</sup>
           </label>
-          <input type="email" name="email" placeholder="example@gmail.com..." />
+          <input
+            type="email"
+            {...register("email", { required: "Field is required!" })}
+            placeholder="example@gmail.com..."
+          />
+        </div>
+        <div className="registration_errors">
+          {errors.email?.message && (
+            <p className="text-red-500">{String(errors.email.message)}</p>
+          )}
         </div>
         <div className="input-field">
           <label>
             Number: <sup>*</sup>
           </label>
-          <input type="text" name="number" placeholder="111 111 111..." />
+          <input
+            type="number"
+            {...register("number", {
+              required: "Field is required!",
+              minLength: {
+                value: 9,
+                message: "Must be 9 number!",
+              },
+              maxLength: 9,
+            })}
+            placeholder="111 111 111..."
+          />
         </div>
+        <div className="registration_errors">
+          {errors.number?.message && (
+            <p className="text-red-500">{String(errors.number.message)}</p>
+          )}
+        </div>
+
         <div className="input-field">
           <label>
             Password: <sup>*</sup>
           </label>
-          <input type="password" name="password" placeholder="sd%$df#6F37^g" />
+          <input
+            type="password"
+            {...register("password", { required: "Field is required!", minLength: {
+                value: 8,
+                message: 'Must be at least 8 character!'
+            } })}
+            placeholder="sd%$df#6F37^g"
+          />
+        </div>
+        <div className="registration_errors">
+          {errors.password?.message && (
+            <p className="text-red-500">{String(errors.password.message)}</p>
+          )}
+        </div>
+
+        <div className="input-field">
+          <input type="submit" />
         </div>
       </form>
     </div>
