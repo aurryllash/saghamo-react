2cb6a28eaeceb8b4361cf1b6e29ec0ac121f06a1
aurryllash <lashatsiklauri77@gmail.com>
2024-08-20 15:08:57 +0400

    Add functinality when 404Error page appears navbar and footer dissapears

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home'
-import Products from './components/pages/Products'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer';
-import { Registration } from './components/pages/Registration';
-import SignIn from './components/pages/SignIn';
-import Error404 from './components/pages/Error404';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Registration } from "./components/pages/Registration";
+import SignIn from "./components/pages/SignIn";
+import Error404 from "./components/pages/Error404";
 
 function App() {
+  const LayOut = ({ children }: { children: React.ReactNode }) => {
+    const location = useLocation();
+
+    const showNavbar = !["/404", "*"].includes(location.pathname);
+
+    return (
+      <>
+        {showNavbar && <Navbar />}
+        {children}
+        {showNavbar && <Footer />}
+      </>
+    );
+  };
 
   return (
     <div className="app">
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home /> } />
-          <Route path='/Products' element={<Products /> } />
-          <Route path='/registration' element={<Registration /> } />
-          <Route path='/signIn' element={<SignIn /> } />
-          <Route path='*' element={<Error404 />} />
-        </Routes>
-        <Footer />
+        <LayOut>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Products" element={<Products />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/404" element={<Error404 />} />
+            <Route path="*" element={<Navigate to={"/404"} />} />
+          </Routes>
+        </LayOut>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
